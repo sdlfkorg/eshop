3df96bce1ae52553ba75c6c44e697cae39bcddcd
sdlfkorg <sdlfkorg@gmail.com>
2017-01-10 20:42:53 +0800

    updated for heroku deployment

--- eshop/settings.py
+++ eshop/settings.py
@@ -127,8 +127,10 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 DEBUG = False
-try:from .local_settings import * except ImportError:
-pass
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
 
