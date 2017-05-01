b076de2c17ce90b3b1e4aceaca924eae24c9dbc7
sdlfkorg <sdlfkorg@gmail.com>
2017-05-01 18:21:25 +0800

    FIX: fix for deploy.

--- eshop/settings.py
+++ eshop/settings.py
@@ -215,7 +215,7 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
-DEBUG = True
+DEBUG = False
 
 try:
     from .local_settings import *
