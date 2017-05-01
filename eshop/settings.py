43a7006d22dd82818648b48e97d23ef08c5435b7
sdlfkorg <sdlfkorg@gmail.com>
2017-05-01 17:50:04 +0800

    FIX: fix for deploy.

--- eshop/settings.py
+++ eshop/settings.py
@@ -195,7 +195,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
@@ -203,7 +203,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 
